2059547cd429f8b3f3c65e92631f9f6ad1d0c117
krishnakeshariya1 <keshariyakrishna8@gmail.com>
2025-11-04 18:51:02 +0530

    01-folder

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,14 @@
+const App = ()=>{
+  return(
+  <>
+    <div id="dad">
+      <h1 id="child1">Hey</h1>
+      <h2 id="child2">hlw</h2>
+    </div>
+    <div id="chacha">
+      <h3 id="cousin">Hello</h3>
+    </div>
+  </>
+  )
+}
+export default App
---
+++ 01-folder/src/main.jsx
@@ -0,0 +1,5 @@
+import * as reactDOM from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+reactDOM.createRoot(document.getElementById('root')).render(<App/>)
