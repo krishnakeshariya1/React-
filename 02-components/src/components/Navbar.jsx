2896197f7fd474b5d8960088734db7ae5b539724
krishnakeshariya1 <keshariyakrishna8@gmail.com>
2025-11-05 17:47:00 +0530

    02-components

---
+++ 02-components/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = ()=>{
+    return (
+        <div>
+            <div className="navbar">
+                <ul>
+                    <li>Home</li>
+                    <li>Home</li>
+                    <li>Home</li>
+                    <li>Home</li>
+                    <li>Home</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+export default Navbar
---
+++ 02-components/src/components/card.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Card() {
+    return(
+        <>
+        <h1>Krishna keshariya</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis mollitia accusantium quod quia voluptatibus quibusdam quam ex suscipit consequatur! Quibusdam mollitia numquam fugit cum molestias officiis possimus similique consectetur dolores!</p>
+        </>
+    )
+}
+export default Card
