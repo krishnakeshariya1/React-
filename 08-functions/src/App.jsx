b17745c9ac8130311a8eae43863c3345124b2967
krishnakeshariya1 <keshariyakrishna8@gmail.com>
2025-11-17 19:20:10 +0530

    08-function

---
+++ 08-functions/src/App.jsx
@@ -0,0 +1,24 @@
+
+const App = ()=>{
+  const btnClick = ()=>{
+    console.log("Hey button clicked");
+  }
+  const clicking = (val)=>{
+    console.log(val)
+  }
+  return(
+    <div>
+      <button onClick={btnClick}>Click me</button>
+      <button onClick={()=>{
+        console.log("clicked")
+      }}> click meee</button>
+
+      <div>
+        <input type="text" placeholder="enter something" onChange={function(element){
+          clicking(element.target.value)
+        }}/>
+      </div>
+    </div>
+  )
+}
+export default App
