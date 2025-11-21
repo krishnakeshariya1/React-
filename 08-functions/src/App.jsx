ea2192cc174109cb066192d204fa701a2cf2bf5c
krishnakeshariya1 <keshariyakrishna8@gmail.com>
2025-11-21 17:24:18 +0530

    functions

--- 08-functions/src/App.jsx
+++ 08-functions/src/App.jsx
@@ -1,22 +1,22 @@
 
-const App = ()=>{
-  const btnClick = ()=>{
+const App = () => {
+  const btnClick = () => {
     console.log("Hey button clicked");
   }
-  const clicking = (val)=>{
+  const clicking = (val) => {
     console.log(val)
   }
-  return(
+  return (
     <div>
       <button onClick={btnClick}>Click me</button>
-      <button onClick={()=>{
+      <button onClick={() => {
         console.log("clicked")
       }}> click meee</button>
 
       <div>
-        <input type="text" placeholder="enter something" onChange={function(element){
+        <input type="text" placeholder="enter something" onChange={function (element) {
           clicking(element.target.value)
-        }}/>
+        }} />
       </div>
     </div>
   )
