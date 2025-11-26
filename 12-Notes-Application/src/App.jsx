62dd8ce285baa2e6314c0ab90c1431f08de017be
krishnakeshariya1 <keshariyakrishna8@gmail.com>
2025-11-26 18:55:41 +0530

    add delete functionality

--- 12-Notes-Application/src/App.jsx
+++ 12-Notes-Application/src/App.jsx
@@ -6,13 +6,22 @@ const App = () => {
   const [detail, setDetail] = useState('');
   const [task, setTask] = useState([]);
 
+
   const submitHandler = (e) => {
     e.preventDefault();
+
     let copyTask = [...task];
     copyTask.push({ title, detail });
+
     setTask(copyTask)
     setTitle('');
     setDetail('');
+  }
+  const deleteNote = (index) => {
+    const copyTask = [...task];
+
+    copyTask.splice(index, 1);
+    setTask(copyTask)
   }
 
   return (
@@ -38,16 +47,22 @@ const App = () => {
           </textarea>
 
           <button className="bg-white rounded text-black px-5 py-2 ">Add Note</button>
+
         </div>
       </form>
       <div className="flex gap-10 p-10 flex-col">
         <h2 className="font-medium text-4xl">Recent Notes</h2>
         <div className="flex flex-wrap gap-20 ">
           {task.map((elem, index) => {
-            return <div key={index} className="h-70 w-60 overflow-hidden text-black bg-[url('https://img.freepik.com/free-vector/leafy-patterned-note-background_53876-97734.jpg?semt=ais_hybrid&w=740&q=80')]
-             rounded-2xl p-10 bg-cover bg-no-repeat">
-              <h3 className="mb-3 font-bold">{elem.title}</h3>
-              <p>{elem.detail}</p>
+            return <div key={index} className="h-70 w-60 overflow-hidden text-black bg-[url('https://i.pinimg.com/474x/00/39/f2/0039f20e0b0ff794870f147d15db9cf5.jpg')]
+             rounded-2xl p-5 bg-cover bg-no-repeat flex flex-col  justify-between">
+              <div className="py-8" >
+                <h3 className="mb-3 font-bold">{elem.title}</h3>
+                <p>{elem.detail}</p>
+              </div>
+              <button className="w-full bg-amber-400 rounded-2xl " onClick={() => {
+                deleteNote(index)
+              }}>Delete Note</button>
             </div>
           })}
         </div>
