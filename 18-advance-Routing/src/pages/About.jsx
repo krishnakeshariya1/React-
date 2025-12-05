f3176da08c465797493ea2c78388d42a348d011c
krishnakeshariya1 <keshariyakrishna8@gmail.com>
2025-12-05 15:39:30 +0530

    done

--- 18-advance-Routing/src/pages/About.jsx
+++ 18-advance-Routing/src/pages/About.jsx
@@ -1,6 +1,15 @@
+import { useNavigate } from "react-router-dom"
+
 const About =()=>{
+    const navigate = useNavigate()
+    
     return(
         <div>
+            <button className="bg-red-600 px-2 py-1 mt-2.5 rounded"
+            onClick={()=>{
+                navigate('/')
+            }}
+            >Return to Home page</button>
             <h1>ABOUT PAGE</h1>
         </div>
     )
